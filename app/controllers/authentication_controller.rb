d58c001ff336e0daf94cc17f512bb0dce487e62b
AiHughesToo <alex@aihughes.dev>
2022-09-29 09:39:07 -0400

    update suth function

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -5,9 +5,9 @@ class AuthenticationController < ApplicationController
       if (user = User.find_by(email: params[:email])).nil?
         render json: { errors: 'User not Found' }, status: :unauthorized
       elsif user.valid_password? params[:password]
-        user.sign_in_count += 1
-        user.save
-        object = {user_type: user.account_type, user_name: user.name, sign_in_count: user.sign_in_count, token: JsonWebToken.encode(sub: user.id) }
+        # user.sign_in_count += 1
+        # user.save
+        object = {user_type: user.account_type, user_f_name: user.f_name, token: JsonWebToken.encode(sub: user.id) }
         render json: object
       else
         render json: { errors: 'unauthorised' }, status: :unauthorized
